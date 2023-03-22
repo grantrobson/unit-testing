550ade23d7d0d9d1bdd40198a6b9ff6f052fa7c7
grantrobson <grant.robson@digital.hmrc.gov.uk>
2023-03-22 11:07:23 +0000

    Reformat

--- src/test/scala/unit/PersonValidatorSpec.scala
+++ src/test/scala/unit/PersonValidatorSpec.scala
@@ -7,40 +7,40 @@ class PersonValidatorSpec extends AnyWordSpec with Matchers {
 
   private val personDetailsValid = PersonDetail("Shehzad", "Ismail", "07666554423")
   private val personDetailsInvalid = PersonDetail("", "Ismail", "07666554423")
-  private val personDetailsInvalid2 = PersonDetail("a"*41, "Ismail", "07666554423")
+  private val personDetailsInvalid2 = PersonDetail("a" * 41, "Ismail", "07666554423")
 
   private val personDetailsValid1 = PersonDetail("Shehzad", "Ismail", "07666554423")
   private val personDetailsInvalid1 = PersonDetail("Shehzad", "", "07666554423")
-  private val personDetailsInvalid21 = PersonDetail("Shehzad", "a"*41, "07666554423")
+  private val personDetailsInvalid21 = PersonDetail("Shehzad", "a" * 41, "07666554423")
   private val personValidator = new PersonValidator()
 
 
-  "test firstname" when {
-    validateName(personDetailsValid, personDetailsInvalid, personDetailsInvalid2)
+  "validate for firstName" must {
+    behave like validateName(personDetailsValid, personDetailsInvalid, personDetailsInvalid2)
   }
 
-  "test surname" when {
-    validateName(personDetailsValid1, personDetailsInvalid1, personDetailsInvalid21)
+  "validate for lastName" must {
+    behave like validateName(personDetailsValid1, personDetailsInvalid1, personDetailsInvalid21)
   }
 
   private def validateName(pd1: PersonDetail, pd2: PersonDetail, pd3: PersonDetail) = {
 
-      "contains at least one character" must {
-        "return Nil as there are no errors" in {
-          personValidator.validate(pd1) mustBe Nil
-        }
-      }
-
-      "contains no characters" must {
-        "return a Seq of one error" in {
-          personValidator.validate(pd2) mustBe Seq("Error")
-        }
-      }
-
-      "contain a maximum of 40 characters" must {
-        "return a Seq of one error" in {
-          personValidator.validate(pd3) mustBe Seq("Error2")
-        }
+    "contains at least one character" must {
+      "return Nil as there are no errors" in {
+        personValidator.validate(pd1) mustBe Nil
       }
     }
+
+    "contains no characters" must {
+      "return a Seq of one error" in {
+        personValidator.validate(pd2) mustBe Seq("Error")
+      }
+    }
+
+    "contain a maximum of 40 characters" must {
+      "return a Seq of one error" in {
+        personValidator.validate(pd3) mustBe Seq("Error2")
+      }
+    }
+  }
 }
